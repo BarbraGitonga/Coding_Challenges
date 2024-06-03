68b8fcfa708f997bd686b6787049fb9b0904ae62
Barbra-Gitonga <gitongabarbra@gmail.com>
2024-06-04 00:44:55 +0300

    solution in python

--- Path_of_Maximum_Gold/Path_of_maximum_gold.py
+++ Path_of_Maximum_Gold/Path_of_maximum_gold.py
@@ -4,9 +4,22 @@ from typing import List
 
 class Solution:
     def getMaximumGold(self, grid: List[List[int]]) -> int:
-        element = grid[][]
-        directions = [(0,1), (1, 0), (-1,0), (0,-1)]
-        for i, j in directions:
+        def dfs(x, y):
+            if not (0 <= x < rows and 0 <= y < cols and grid[x][y]):
+                return 0
+
+            element = grid[x][y]
+            grid[x][y] = 0
+
+            current_max_gold = element + max(
+                dfs(x + dx, y + dy) for dx, dy in dir
+            )
             
-    def search():
+            grid[x][y] = element 
+
+            return current_max_gold
         
+        rows, cols = len(grid), len(grid[0])
+        dir = [(0, 1), (0, -1), (-1, 0), (1, 0)]
+        
+        return max(dfs(x, y) for x in range(len(grid)) for y in range(len(grid[0])))
