21372f8f9f31df86716ee10fb50f6c1e9f26e4f0
Barbra Gitonga <gitongabarbra@gmail.com>
2024-06-25 18:42:11 +0300

    seg fault

---
+++ Advent_of_code/day1/part2.c
@@ -0,0 +1,85 @@
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdio.h>
+
+int findigits(char* window){
+    const char *words[] = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+    char nums[10] = {'1', '2', '3', '4', '5', '6', '7', '8', '9'};
+
+    for (int i=0; i<10; i++){
+        const char a = nums[i];
+
+        if(strncmp(window, words[i], 3) == 0 || strncmp(window, words[i], 4) == 0 || strncmp(window, words[i], 5) == 0){
+              return nums[i] -'0';
+        }
+        else if(window[0] == a){
+            return window[0] - '0';
+        }
+    }
+    return -1;
+}
+
+char* window(char *lines){
+    char *window_string = malloc(6 * sizeof(char)); // Allocate memory for 5 chars + null terminator
+    strncpy(window_string, lines, 5); // Copy the first 5 characters
+    window_string[5] = '\0';
+    
+    return window_string;
+}
+int tens_first(char* lines){
+    int len = strlen(lines);
+    int first = 0;
+
+    for(int i = 0; i<len; i++){
+        char wind[5];
+        strcpy(wind, window(lines));
+        if(findigits(wind) != -1){
+            first = findigits(wind);
+            break;
+        } 
+    }
+    return first;
+}
+
+int last_ones(char* lines){
+    int len = strlen(lines);
+    int last = 0;
+
+    for(int i = 0; i<len; i++){
+        char wind[5];
+        strcpy(wind, window(lines));
+        if (findigits(wind) != -1){
+            last = findigits(wind);
+            break;
+        } 
+    }
+    return last;
+}
+
+int search(const char *filepath){
+    FILE* file = fopen(filepath, "r");
+    char lines[100];
+    char count = 0;
+    int sum = 0;
+    int fsum, lsum;
+
+    if (file == NULL){
+        printf("file is NULL \n");
+    }
+
+    while (fgets(lines, sizeof(lines), file)){
+        fsum += tens_first(lines);
+        lsum += last_ones(lines);
+    }
+    sum = (fsum * 10) + lsum;
+    fclose(file);
+    return sum;
+}
+
+int main(void){
+  const char *filepath = "callibaration.txt";
+  int total = search(filepath);
+  printf("Total = %d", total);
+  return 0;
+}
