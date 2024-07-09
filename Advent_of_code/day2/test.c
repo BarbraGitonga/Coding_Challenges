791232b534c8e37e51f76385116009b3c61c839e
Barbra Gitonga <gitongabarbra@gmail.com>
2024-07-09 05:42:26 +0300

    logic error

--- Advent_of_code/day2/test.c
+++ Advent_of_code/day2/test.c
@@ -4,59 +4,45 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-int sections(char *line){
-    char semi = ";";
-    int len = strlen(line);
-    int count = 0;
-
-    for (int i=7; i<len; i++){
-        if(line[i] == semi){
-            count++;
-        }
-    }
-    return count;
-}
-bool game_match(char *line){
+int game_match(char *line){
     char *color[] = {"red", "green", "blue"}; //12, 13, 14
     int len = strlen(line);
     printf("len: %d", len);
     char var[3];
     int num = 0;
-    bool red, blue, green = false;
-    int semi = section(line);
-    
-    for(int j=0; j<=semi; j++){
-        for (int i=7; i<len;i++){
-            if (isdigit((unsigned char)line[i])) {
-                // Copy up to 2 characters starting from line[i] into var
-                strncpy(var, line + i, 2);
-                var[2] = '\0'; // Ensure null termination
-                printf("var: %s", var);
-                num = atoi(var);
-                printf("num: %d", num);
-                i+=2;
-                if(strncmp(line+i, color[0], 3) == 0 != num<=12){
-                    printf("red");
-                    break;
-                }
-                if(strncmp(line+i, color[1], 5) == 0 !=  num<=13){
-                    printf("green");
-                    break;
-                }
-                if(strncmp(line+i, color[2], 4) == 0 && num<=14){
-                    printf("blue");
-                    blue = true;
-                }
+    int red, blue, green =0;
+   
+   
+    for (int i=7; i<len; i++){
+        if (isdigit((unsigned char)line[i])){
+            // Copy up to 2 characters starting from line[i] into var
+            strncpy(var, line + i, 2);
+            var[2] = '\0'; // Ensure null termination
+            printf("var: %s", var);
+            num = atoi(var);
+            printf("num: %d", num);
+            i+=2;
+            if(strncmp(line+i, color[0], 3) == 0 && num<=12){
+                printf("red");
+                red = 1;
             }
+            if(strncmp(line+i, color[1], 5) == 0 &&  num<=13){
+                printf("green");
+                green = 1;
+            }
+            if(strncmp(line+i, color[2], 4) == 0 && num<=14){
+                printf("blue");
+                blue = 1;
+            }
+            
+    }
+    if (red==0 || green==0 || blue==0){
+        printf("all 0");
+        return 0;
         }
+    printf( "1");
+    return 1;
     }
-    
-    if (red == true && blue == true && green == true){
-        printf("all true");
-        return true;
-    }
-    printf("false");
-    return false;
 }
 
 int total_games(const char *filepath){
@@ -71,13 +57,9 @@ int total_games(const char *filepath){
 
     while (fgets(lines, sizeof(lines), file)){
         printf("here");
-        if (game_match(lines) == false){
-            printf("inside");
-            break;
-        }
-        else{
+        if (game_match(lines) == 1){
             total = games+total;
-            printf("total: %d", total);
+            printf("total: %d\n", total);
             games++;
         }
     }
@@ -86,9 +68,9 @@ int total_games(const char *filepath){
     return total;
 }
 int main(void){
-    const char *filepath = "Game.txt";
+    const char *filepath = "test.txt";
 
     int match = total_games(filepath);
-    printf("Total games: %d", match);
+    printf("Total games: %d\n", match);
     return 0;
 }
