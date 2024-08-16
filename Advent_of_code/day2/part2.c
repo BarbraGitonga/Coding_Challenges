cd39d6d73f119625982dfe998d0b220c2fc7baef
Barbra-Gitonga <gitongabarbra@gmail.com>
2024-08-16 15:41:01 +0300

    part 2 complete

--- Advent_of_code/day2/part2.c
+++ Advent_of_code/day2/part2.c
@@ -6,29 +6,47 @@
 
 int min_multiplied(char* line) {
     char *games_start = strchr(line, ':') + 1;
-    char *token, *subtoken;
-    int max_red = 0, max_blue = 0, max_green = 0, num = 0;
+    int max_red = 1, max_blue = 1, max_green = 1;
 
     printf("Processing line: %s", line);  // Debug print
 
-    for (token = strtok(games_start, ";"); token != NULL; token = strtok(NULL, ";")) {
-        for (subtoken = strtok(token, ","); subtoken != NULL; subtoken = strtok(NULL, ",")) {
-            char color[6];
-            sscanf(subtoken, "%d %s", &num, color);
-            
-            printf("Found: %d %s\n", num, color);  // Debug print
+    char *game_copy = strdup(games_start);  // Create a copy of the game string
+    char *game_ptr = game_copy;  // Pointer to free later
 
-            if (strcmp(color, "red") == 0 && num > max_red) max_red = num;
-            if (strcmp(color, "blue") == 0 && num > max_blue) max_blue = num;
-            if (strcmp(color, "green") == 0 && num > max_green) max_green = num;
+    char *set = strtok(game_copy, ";");
+    while (set != NULL) {
+        char *cube = set;
+        while (*cube) {
+            while (*cube && isspace(*cube)) cube++;  // Skip leading spaces
+            if (!*cube) break;
+
+            int num;
+            char color[6] = {0};  // Initialize to zero
+            if (sscanf(cube, "%d %5[^,;\n]", &num, color) == 2) {
+                // Remove trailing comma if present
+                char *comma = strchr(color, ',');
+                if (comma) *comma = '\0';
+                
+                printf("Found: %d %s\n", num, color);  // Debug print
+
+                if (strcmp(color, "red") == 0 && num > max_red) max_red = num;
+                else if (strcmp(color, "blue") == 0 && num > max_blue) max_blue = num;
+                else if (strcmp(color, "green") == 0 && num > max_green) max_green = num;
+            }
+
+            // Move to the next cube in this set
+            while (*cube && *cube != ',') cube++;
+            if (*cube == ',') cube++;
         }
+        set = strtok(NULL, ";");
     }
+    
+    free(game_ptr);  // Free the copied string
     
     printf("Max values: red=%d, green=%d, blue=%d\n", max_red, max_green, max_blue);  // Debug print
     
     return max_blue * max_green * max_red;
 }
-
 int summation(const char *filepath) {
     FILE *file = fopen(filepath, "r");
     char line[256];
