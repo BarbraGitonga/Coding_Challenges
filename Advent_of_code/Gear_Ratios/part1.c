d84d1d162242343cda1fa21382449207b62dc21b
Barbra-Gitonga <gitongabarbra@gmail.com>
2024-09-16 08:09:31 +0300

    gear ratios

---
+++ Advent_of_code/Gear_Ratios/part1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+int part_number(char){
+    int dx[] = {-1, 1, 0, 0}; // x coordinates
+    int dy[] = {0, 0, -1, 1}; // y coordinates
+    for ()
+}
+int line_divider(const char*filename) {
+    char line[131];
+    int sum = 0;
+    while (fgets(line, sizeof line, filename) != NULL){
+        
+    }
+    
+}
